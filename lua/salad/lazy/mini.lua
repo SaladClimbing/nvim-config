fe390f02ed361493a219ac7bd2ff717741b25ba8
SaladClimbing <xxthegamingsaldxx@gmail.com>
2026-07-25 17:48:14 -0400

    refactor: replace nvim-cmp and autopairs with mini.nvim modules
    Use mini.completion, mini.pairs, and mini.snippets instead of nvim-cmp
    ecosystem. Comment out backspace and delete keymaps in hardmode to
    allow basic editing.

--- lua/salad/lazy/mini.lua
+++ lua/salad/lazy/mini.lua
@@ -4,6 +4,16 @@ return {
 	config = function()
 		-- Text Editing --
 		require("mini.comment").setup()
+		require("mini.completion").setup({
+			mappings = {
+				force_twostep = "<C-Space>",
+				force_fallback = "<A-Space>",
+				scroll_down = "<C-f>",
+				scroll_up = "<C-b>",
+			},
+		})
+		require("mini.pairs").setup()
+		require("mini.snippets").setup()
 		require("mini.surround").setup({
 			mappings = {
 				add = "sa",
@@ -21,5 +31,14 @@ return {
 		-- Appearance --
 
 		-- Misc. --
+
+		-- Override <CR> to confirm completion --
+		vim.keymap.set("i", "<CR>", function()
+			if vim.fn.pumvisible() == 1 then
+				return "<C-y>"
+			else
+				return "<CR>"
+			end
+		end, { expr = true })
 	end,
 }
