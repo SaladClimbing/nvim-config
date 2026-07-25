fb5d4a65bf3bfef799b5ba4765265a18a4a6537c
SaladClimbing <xxthegamingsaldxx@gmail.com>
2026-07-25 17:18:59 -0400

    refactor: replace Comment.nvim with mini.comment, add mini.surround

--- lua/salad/lazy/mini.lua
+++ lua/salad/lazy/mini.lua
@@ -1,4 +1,25 @@
 return {
 	"nvim-mini/mini.nvim",
 	version = "*",
+	config = function()
+		-- Text Editing --
+		require("mini.comment").setup()
+		require("mini.surround").setup({
+			mappings = {
+				add = "sa",
+				delete = "sd",
+				find = "sf",
+				find_left = "sF",
+				highlight = "sh",
+				replace = "sr",
+				update_n_lines = "sn",
+			},
+		})
+
+		-- General Workflow --
+
+		-- Appearance --
+
+		-- Misc. --
+	end,
 }
